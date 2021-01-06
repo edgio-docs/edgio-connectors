43a5bb0cd6e6b8a3b90f28e51701cd9099fdfc5e
Tristan Lee <tristanlee85@gmail.com>
2021-01-06 13:20:53 -0500

    Remove duplicate files

--- xdn-nuxt-connector/dev.ts
+++
@@ -1,9 +0,0 @@
-import createDevServer from '@xdn/core/dev/createDevServer'
-
-export default async function dev() {
-  return createDevServer({
-    label: 'Nuxt',
-    command: () => 'npx nuxt dev',
-    ready: [/Listening on/i, /Listening on/i],
-  })
-}
